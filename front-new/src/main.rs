1a64903ce853ad92892f81ad0af3de66ef248d68
confucius40 <git@mail.katon15.xyz>
2026-08-17 12:26:05 +0200

    basic boiletplate

--- front-new/src/main.rs
+++ front-new/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+
+slint::include_modules!();
+
+#[cfg_attr(
+    target_arch = "wasm32",
+    wasm_bindgen(start)
+)]
+pub fn main() -> Result<(), slint::PlatformError> {
+    let app = MainWindow::new()?;
+
+    app.run()
 }
+
+
